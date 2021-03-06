0eb8e0663b8b98a615e60a2e9962bc749bc4b2cf
joe-irving <joeirving.jbi@gmail.com>
2021-03-06 21:48:03 +0000

    Added a cool dude

--- functions.php
+++ functions.php
@@ -19,10 +19,22 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'twentytwentyone-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'cop26theme-style', get_stylesheet_directory_uri() . '/style.css', array( 'twentytwentyone-style' ) );
 }
-
-
 // Custom Function to add icon
 function my_favicon_link() {
     echo '<link rel="icon" type="image/x-icon" href="/wp-content/themes/cop26-child-theme/images/favicon.ico" />' . "\n";
 }
 add_action( 'wp_head', 'my_favicon_link' );
+
+// I was tring to make the default icons our custom ones, gave up
+
+// function mytheme_includes() {
+// 	require_once get_theme_file_path( 'classes/svg_icons.php' );
+// }
+// add_action( 'after_setup_theme', 'mytheme_includes' );
+//
+//
+// function child_init() {
+//   $child_svg_icons = new child_svg_icons();
+//   $child_svg_icons->change_icons();
+// }
+// add_action( 'init', 'child_init' );
